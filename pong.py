a82ff686abe7bcb95a6ae1f25e795a5ecb2875c8
Alan <alan.gutierrez@cetys.edu.mx>
2020-10-24 08:43:23 -0700

    Otra version del juego

--- pong.py
+++ pong.py
@@ -1,3 +1,21 @@
-print("Hola Mundo")
-print("Otro texto")
-print("Otra linea")
+import pygame, sys
+
+pygame.init()
+clock = pygame.time.Clock()
+
+screen_width = 1080
+screen_height = 680
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_capation("Mi juego de Pong")
+
+ball=pygame.Rect(screen_width/2 - 11, screen_height -11,22,22)
+
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+
+
+	pygame.display.flip()
+	clock.tick(60)
